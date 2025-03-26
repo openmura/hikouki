7c36e8bccc887a1141f991a451bbfdba9d6d317a
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-03-26 16:20:44 +0900

    scriptが上書きされていた問題の解決

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ function reAlert() {
     localStorage.removeItem("visited");
     window.location.reload();
 }
+
+function randomWisdom() {
+    const maxVol = 11;
+    const randomVol = Math.floor(Math.random() * maxVol) + 1;
+    window.location.href = `vol/${randomVol}.html`;
+}
