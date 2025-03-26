5337b9fbc44f847b9d2053e25bd0b25d4213744e
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-03-26 15:20:45 +0900

    使用上の注意を表示

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function randomWisdom() {
     const randomVol = Math.floor(Math.random() * maxVol) + 1;
     window.location.href = `vol/${randomVol}.html`;
 }
+window.onload = function() {
+    if (localStorage.getItem("visited")) {
+        console.log("以前アクセスしたことがあります。");
+    } else {
+        alert("使用上の注意\n\n・無断転載禁止\n・暇な時に見ること");
+        localStorage.setItem("visited", "true");
+    }
+};
